1a98025dc9a1d019e4c91bee886c70bcada6030f
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-09 18:21:41 +0800

    integrating create post

---
+++ client/src/PostCreate.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:4000/posts", { title });
+    if (response) {
+      setTitle("");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default PostCreate;
