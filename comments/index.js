8fb4f78f9ecbae77e8adfa09a95c0ca77bcfee13
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-10 16:03:54 +0800

    emitting event from comments service

--- comments/index.js
+++ comments/index.js
@@ -2,26 +2,38 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const EVENT_BUS = "http://localhost:4005/events";
 const commentsByPostId = {};
 
 app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comment", (req, res) => {
+app.post("/posts/:id/comment", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
-
-  const comments = commentsByPostId[req.params.id] || [];
+  const postId = req.params.id;
+  const comments = commentsByPostId[postId] || [];
 
   comments.push({ id: commentId, content });
 
-  commentsByPostId[req.params.id] = comments;
+  commentsByPostId[postId] = comments;
+
+  const event = {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId,
+    },
+  };
+  await axios.post(EVENT_BUS, event);
 
   res.status(201).send(comments);
 });
