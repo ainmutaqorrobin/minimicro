6cde8e88f03a12f318bb3fadf10ec0f1d12c45a2
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-10 15:44:57 +0800

    add event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+const ENDPOINT = {
+  POST_SERVICE: "http://localhost:4000/events",
+  COMMENT_SERVICE: "http://localhost:4001/events",
+  QUERY_SERVICE: "http://localhost:4002/events",
+};
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios
+    .post(ENDPOINT.POST_SERVICE, event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post(ENDPOINT.COMMENT_SERVICE, event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post(ENDPOINT.QUERY_SERVICE, event)
+    .catch((err) => console.log(err.message));
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on port 4005");
+});
