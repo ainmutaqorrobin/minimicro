147cc0520e7520bb9d29862804769b343a519e79
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-10 15:56:21 +0800

    emitting event from posts service

--- posts/index.js
+++ posts/index.js
@@ -2,22 +2,27 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const EVENT_BUS = "http://localhost:4005/events";
 const posts = {};
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
   posts[id] = { id, title };
+
+  const event = { type: "PostCreated", data: posts[id] };
+  await axios.post(EVENT_BUS, event);
 
   res.status(201).send(posts[id]);
 });
