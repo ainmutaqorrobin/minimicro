6c9041d2f8f3ee4d4db19a039b4e0739625b4595
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-07-11 12:23:04 +0800

    integrating comment by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,7 +1,19 @@
 function CommentList({ comments }) {
-  const renderedComments = comments.map((comment) => (
-    <li key={comment.id}>{comment.content}</li>
-  ));
+  const renderedComments = comments.map((comment) => {
+    const statusMessages = {
+      pending: "This comment is awaiting moderation",
+      rejected: "This comment has been rejected.",
+    };
+
+    const content =
+      comment.status === "approved" ? (
+        comment.content
+      ) : (
+        <p className="font-weight-light">{statusMessages[comment.status]}</p>
+      );
+
+    return <li key={comment.id}>{content}</li>;
+  });
 
   return <ul>{renderedComments}</ul>;
 }
